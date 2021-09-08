0649dd08fb64d234c348436dc8743762276b1485
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2021-09-07 22:04:49 -0300

    feat: categories route and repository

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 
+import { categoriesRoutes } from './routes/categories.routes'
+
 const app = express()
 
-app.get('/', (req, res) => console.log('Get'))
+app.use(express.json())
+
+app.use('/categories', categoriesRoutes)
 
 app.listen(3333, () => console.log('Server is running...'))
