62db013ebfae7250b930796dfcc049dbb881de03
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2021-09-15 20:21:32 -0300

    feat: SOLID

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,27 @@
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository'
+
+interface IRequest {
+  name: string
+  description: string
+}
+
+/**
+ * - Definir o tipo de retorno
+ * - Alterar o retorno de erro
+ * - Acessar o repositório
+ */
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+  execute({ description, name }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name)
+
+    if (categoryAlreadyExists) {
+      throw new Error('Category already exists')
+    }
+
+    this.categoriesRepository.create({ name, description })
+  }
+}
+
+export { CreateCategoryService }
