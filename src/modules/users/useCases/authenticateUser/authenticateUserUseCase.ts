01c4a115c511dfbe657c19b98c47a50fa9983b08
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2021-11-25 09:16:14 -0300

    add token for authentication

---
+++ src/modules/users/useCases/authenticateUser/authenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+import { inject, injectable } from 'tsyringe'
+
+import { IUsersRepository } from '../../repositories/IUsersRepository'
+
+interface IRequest {
+  email: string
+  password: string
+}
+
+interface IResponse {
+  user: {
+    name: string
+    email: string
+  }
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    // usuário existe
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error('Email or password incorrect!')
+    }
+
+    // senha está correta
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!')
+    }
+
+    const token = sign({}, 'chaveSecreta', {
+      subject: user.id,
+      expiresIn: '1d'
+    })
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email
+      }
+    }
+
+    return tokenReturn
+
+    // gerar JasonWebToken
+  }
+}
+
+export { AuthenticateUserUseCase }
