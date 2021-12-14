eafa9a9929743ca238ee8d9fb5f24d2ca44bba76
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2021-12-14 09:46:14 -0300

    feat: category create unit test

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,8 +1,50 @@
-describe('Criar categoria', () => {
-  it('Espero que 2 + 2 seja 4', () => {
-    const soma = 2 + 2
-    const resultado = 5
+import { AppError } from '../../../../errors/AppError'
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRespositoryInMemory'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
 
-    expect(soma).toBe(resultado)
+let createCategoryUseCase: CreateCategoryUseCase
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    )
+  })
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Category description Teste'
+    }
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description
+    })
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    )
+
+    expect(categoryCreated).toHaveProperty('id')
+  })
+
+  it('should not be able to create a new category with existent name', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category Test',
+        description: 'Category description Teste'
+      }
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description
+      })
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description
+      })
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
