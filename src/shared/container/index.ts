f7e3fc1e82be23a3d0b547a3db8c14e49ba24d0a
Daniel Carvalho <danielborgesdecarvalho@gmail.com>
2022-01-17 09:08:18 -0300

    create rentals controller and repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,13 @@
 import { container } from 'tsyringe'
 
+import '@shared/container/providers'
+
 import { CarImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarImagesRepository'
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository'
 import { ICarsImageRepository } from '@modules/cars/repositories/ICarImagesRepository'
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
+import { RentalsRepository } from '@modules/rentals/infra/typeorm/repositories/RentalsRepository'
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository'
 
 import { CategoriesRepository } from '../../modules/cars/infra/typeorm/repositories/CategoriesRepository'
 import { SpecificationsRepository } from '../../modules/cars/infra/typeorm/repositories/SpecificationsRepository'
@@ -33,4 +37,9 @@ container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository)
 container.registerSingleton<ICarsImageRepository>(
   'CarsImagesRepository',
   CarImagesRepository
+)
+
+container.registerSingleton<IRentalsRepository>(
+  'RentalsRepository',
+  RentalsRepository
 )
